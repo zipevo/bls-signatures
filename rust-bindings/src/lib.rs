88ee9d05197351dd35e4bb626afbe365218e914c
Ivan Shumkov <ivan@shumkov.ru>
2022-08-25 16:55:30 +0300

    chore: WIP

--- rust-bindings/src/lib.rs
+++ rust-bindings/src/lib.rs
@@ -1,4 +1,10 @@
-// extern crate core;
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 
-// pub mod elements;
+pub mod elements;
 // mod bindings;
+
+
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
