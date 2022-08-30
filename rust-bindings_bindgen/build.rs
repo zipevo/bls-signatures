965ac58df471cdfb9057b3d86122fae9d456f5fa
Ivan Shumkov <ivan@shumkov.ru>
2022-08-30 03:07:22 +0300

    build: automate build process part 2

--- rust-bindings_bindgen/build.rs
+++ rust-bindings_bindgen/build.rs
@@ -1,6 +1,7 @@
 use std::{env, fs, io};
 use std::borrow::{Borrow, BorrowMut};
 use std::collections::HashSet;
+use std::env::args;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Write};
 use std::ops::Deref;
@@ -34,47 +35,62 @@ fn create_cross_cmake_command() -> Command {
 const BUILD_PATH: &str = "../build";
 
 fn main() {
-    // Prepare build
+    // Run cmake
     fs::remove_dir_all(BUILD_PATH).expect("can't clean build directory");
     fs::create_dir_all(BUILD_PATH).expect("can't create build directory");
 
     let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
 
-    create_cross_cmake_command()
+    let cmake_output = create_cross_cmake_command()
         .current_dir(abs(BUILD_PATH))
+        .arg("-DBUILD_BLS_PYTHON_BINDINGS=0")
+        .arg("-DBUILD_BLS_TESTS=0")
+        .arg("-DBUILD_BLS_BENCHMARKS=0")
+        .arg("-DBUILD_BLS_JS_BINDINGS=0")
         .arg("..")
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .status()
-        .expect("should prepare build files");
+        .output()
+        .expect("can't run cmake");
 
-    // Build deps
-    Command::new("cmake")
+    io::stdout().write_all(&cmake_output.stdout).expect("should write output");
+    io::stderr().write_all(&cmake_output.stderr).expect("should write output");
+
+    // Build deps for bls-signatures
+    let build_output = Command::new("cmake")
         .args(["--build", ".", "--", "-j", "6"])
         .current_dir(BUILD_PATH)
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .status()
-        .expect("should prepare build files");
+        .output()
+        .expect("can't build bls-signatures deps");
+
+    io::stdout().write_all(&build_output.stdout).expect("should write output");
+    io::stderr().write_all(&build_output.stderr).expect("should write output");
 
     // Collect include paths
-    // create_cross_cmake_command()
+    let include_paths_file_path = PathBuf::from(BUILD_PATH)
+        .join("include_paths.txt");
 
+    let include_paths = fs::read_to_string(include_paths_file_path)
+        .expect("should read include paths from file");
 
-    // Command::new("")
+    let mut include_paths: Vec<_> = include_paths.split(';').into_iter()
+        .filter(|path| !path.is_empty())
+        .collect();
+
+    let relic_src = abs("../build/_deps/relic-src/include");
+    let relic_build = abs("../build/_deps/relic-build/include");
+    let build_src = abs("../build/src");
+    let src = abs("../src");
+
+    include_paths.extend([
+        relic_src.as_str(),
+        relic_build.as_str(),
+        build_src.as_str(),
+        src.as_str(),
+    ]);
 
     let mut cc = cc::Build::new();
 
-    let library_path = Path::new("../go-bindings");
-
-    let cpp_files = [
-        abs("../go-bindings/blschia.cpp"),
-        // "../go-bindings/elements.cpp",
-        // "../go-bindings/privatekey.cpp",
-        // "../go-bindings/schemes.cpp",
-        // "../go-bindings/threshold.cpp",
-        // "../go-bindings/utils.cpp",
-    ];
+    let cpp_files: Vec<_> = glob::glob("c_binding/*.cpp")
+        .expect("can't get list of cpp files").filter_map(Result::ok).collect();
     // [
     //     abs("../js_build/_deps/relic-src/include").as_str(),
     //     abs("../js_build/_deps/relic-build/include").as_str(),
@@ -90,37 +106,37 @@ fn main() {
     //     "/opt/homebrew/include/",
     // ]
     //
-    // cc.files(cpp_files)
-    //     .includes(&include_paths)
-    //     .flag_if_supported("-std=c++14");
-    //     // .define("BLSALLOC_SODIUM", Some("1"))
-    //     // .define("SODIUM_STATIC", Some("1"));
-    //
-    // if target_arch.eq("wasm32") {
-    //     cc.flag_if_supported("-ffreestanding")
-    //         .define("_LIBCPP_HAS_NO_THREADS", Some("1"));
-    // } else {
-    //     cc.cpp(true);
-    // }
-    //
-    // if !cfg!(debug_assertions) {
-    //     cc.opt_level(2);
-    // }
-    //
-    // cc.compile("bls-go-binding");
-    //
-    // println!("cargo:rustc-link-search={}", abs("../js_build/_deps/sodium-build"));
-    // println!("cargo:rustc-link-lib=static=sodium");
-    //
-    // println!("cargo:rustc-link-search={}", abs("../js_build/_deps/relic-build/lib"));
-    // println!("cargo:rustc-link-lib=static=relic_s");
-    //
-    // println!("cargo:rustc-link-search=/opt/homebrew/lib");
-    // println!("cargo:rustc-link-lib=static=gmp");
-    //
-    // println!("cargo:rustc-link-search={}", abs("../js_build/src"));
-    // println!("cargo:rustc-link-lib=static=bls-dash");
-    //
+    cc.files(cpp_files)
+        .includes(&include_paths)
+        .flag_if_supported("-std=c++14");
+        // .define("BLSALLOC_SODIUM", Some("1"))
+        // .define("SODIUM_STATIC", Some("1"));
+
+    if target_arch.eq("wasm32") {
+        cc.flag_if_supported("-ffreestanding")
+            .define("_LIBCPP_HAS_NO_THREADS", Some("1"));
+    } else {
+        cc.cpp(true);
+    }
+
+    if !cfg!(debug_assertions) {
+        cc.opt_level(2);
+    }
+
+    cc.compile("bls-go-binding");
+
+    println!("cargo:rustc-link-search={}", abs("../build/_deps/sodium-build"));
+    println!("cargo:rustc-link-lib=static=sodium");
+
+    println!("cargo:rustc-link-search={}", abs("../build/_deps/relic-build/lib"));
+    println!("cargo:rustc-link-lib=static=relic_s");
+
+    println!("cargo:rustc-link-search=/opt/homebrew/lib");
+    println!("cargo:rustc-link-lib=static=gmp");
+
+    println!("cargo:rustc-link-search={}", abs("../build/src"));
+    println!("cargo:rustc-link-lib=static=bls-dash");
+
     // // Create and write binding to src/bindings.rs
     // if env::var("GENERATE_BINDING").is_ok() {
     //     let bindings = bindgen::Builder::default()
