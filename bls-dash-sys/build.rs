92ffd63c28edbd50a732d78b137a183f17f4293e
Ivan Shumkov <ivan@shumkov.ru>
2022-08-31 19:47:49 +0300

    refactor: cargo build script

--- bls-dash-sys/build.rs
+++ bls-dash-sys/build.rs
@@ -1,20 +1,7 @@
 use std::io::Write;
 use std::path::{Path, PathBuf};
-use std::process::Command;
+use std::process::{Command, Output};
 use std::{env, fs, io};
-
-fn abs(path: &str) -> String {
-    let path_buf = PathBuf::from(path);
-
-    let path_abs = path_buf
-        .canonicalize()
-        .expect("should provide valid abs path");
-
-    path_abs
-        .to_str()
-        .expect("should convert path to string")
-        .to_owned()
-}
 
 fn create_cross_cmake_command() -> Command {
     let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
@@ -32,21 +19,45 @@ fn create_cross_cmake_command() -> Command {
     command
 }
 
+fn handle_command_output(output: Output) {
+    io::stdout()
+        .write_all(&output.stdout)
+        .expect("should write output");
+
+    io::stderr()
+        .write_all(&output.stderr)
+        .expect("should write output");
+
+    assert!(output.status.success());
+}
+
 const BUILD_PATH: &str = "../build";
 
 fn main() {
+    let bls_dash_build_path = Path::new("../build")
+        .canonicalize()
+        .expect("can't get abs path");
+
+    let bls_dash_src_path = Path::new("../src")
+        .canonicalize()
+        .expect("can't get abs path");
+
+    let c_binding_path = Path::new("c_binding")
+        .canonicalize()
+        .expect("can't get abs path");
+
     // Run cmake
 
     println!("Run cmake:");
 
-    if Path::new(BUILD_PATH).exists() {
-        fs::remove_dir_all(BUILD_PATH).expect("can't clean build directory");
+    if bls_dash_build_path.exists() {
+        fs::remove_dir_all(&bls_dash_build_path).expect("can't clean build directory");
     }
 
-    fs::create_dir_all(BUILD_PATH).expect("can't create build directory");
+    fs::create_dir_all(&bls_dash_build_path).expect("can't create build directory");
 
     let cmake_output = create_cross_cmake_command()
-        .current_dir(abs(BUILD_PATH))
+        .current_dir(&bls_dash_build_path)
         .arg("-DBUILD_BLS_PYTHON_BINDINGS=0")
         .arg("-DBUILD_BLS_TESTS=0")
         .arg("-DBUILD_BLS_BENCHMARKS=0")
@@ -55,15 +66,7 @@ fn main() {
         .output()
         .expect("can't run cmake");
 
-    io::stdout()
-        .write_all(&cmake_output.stdout)
-        .expect("should write output");
-
-    io::stderr()
-        .write_all(&cmake_output.stderr)
-        .expect("should write output");
-
-    assert!(cmake_output.status.success());
+    handle_command_output(cmake_output);
 
     // Build deps for bls-signatures
 
@@ -75,38 +78,25 @@ fn main() {
         .output()
         .expect("can't build bls-signatures deps");
 
-    io::stdout()
-        .write_all(&build_output.stdout)
-        .expect("should write output");
-
-    io::stderr()
-        .write_all(&build_output.stderr)
-        .expect("should write output");
-
-    assert!(build_output.status.success());
+    handle_command_output(build_output);
 
     // Collect include paths
-    let include_paths_file_path = PathBuf::from(BUILD_PATH).join("include_paths.txt");
+    let include_paths_file_path = bls_dash_build_path.join("include_paths.txt");
 
     let include_paths =
         fs::read_to_string(include_paths_file_path).expect("should read include paths from file");
 
     let mut include_paths: Vec<_> = include_paths
         .split(';')
-        .into_iter()
         .filter(|path| !path.is_empty())
+        .map(|path| PathBuf::from(path))
         .collect();
 
-    let relic_src = abs("../build/_deps/relic-src/include");
-    let relic_build = abs("../build/_deps/relic-build/include");
-    let build_src = abs("../build/src");
-    let src = abs("../src");
-
     include_paths.extend([
-        relic_src.as_str(),
-        relic_build.as_str(),
-        build_src.as_str(),
-        src.as_str(),
+        bls_dash_build_path.join("_deps/relic-src/include"),
+        bls_dash_build_path.join("_deps/relic-build/include"),
+        bls_dash_build_path.join("src"),
+        bls_dash_src_path.clone(),
     ]);
 
     // Build c binding
@@ -115,7 +105,9 @@ fn main() {
 
     let mut cc = cc::Build::new();
 
-    let cpp_files: Vec<_> = glob::glob("c_binding/*.cpp")
+    let cpp_files_mask = c_binding_path.join("*.cpp");
+
+    let cpp_files: Vec<_> = glob::glob(cpp_files_mask.to_str().unwrap())
         .expect("can't get list of cpp files")
         .filter_map(Result::ok)
         .collect();
@@ -146,26 +138,33 @@ fn main() {
     // Link dependencies
     println!(
         "cargo:rustc-link-search={}",
-        abs("../build/_deps/sodium-build")
+        bls_dash_build_path.join("_deps/sodium-build").display()
     );
 
     println!("cargo:rustc-link-lib=static=sodium");
 
     println!(
         "cargo:rustc-link-search={}",
-        abs("../build/_deps/relic-build/lib")
+        bls_dash_build_path.join("_deps/relic-build/lib").display()
     );
 
     println!("cargo:rustc-link-lib=static=relic_s");
 
+    println!(
+        "cargo:rustc-link-search={}",
+        bls_dash_build_path.join("src").display()
+    );
+
+    println!("cargo:rustc-link-lib=static=bls-dash");
+
     // Link GMP if exists
-    let gmp_libraries_file_path = PathBuf::from(BUILD_PATH).join("gmp_libraries.txt");
+    let gmp_libraries_file_path = bls_dash_build_path.join("gmp_libraries.txt");
 
     if gmp_libraries_file_path.exists() {
-        let gmp_libraries_string = fs::read_to_string(gmp_libraries_file_path)
-            .expect("should read gmp includes from file");
-
-        let gmp_libraries_path = PathBuf::from(gmp_libraries_string);
+        let gmp_libraries_path = PathBuf::from(
+            fs::read_to_string(gmp_libraries_file_path)
+                .expect("should read gmp includes from file"),
+        );
 
         let gmp_libraries_parent_path = gmp_libraries_path
             .parent()
@@ -179,21 +178,18 @@ fn main() {
         println!("cargo:rustc-link-lib=static=gmp");
     }
 
-    println!("cargo:rustc-link-search={}", abs("../build/src"));
-    println!("cargo:rustc-link-lib=static=bls-dash");
-
     // Generate rust code for c binding to src/lib.rs
     if env::var("GENERATE_C_BINDING").is_ok() {
         println!("Generate C binding for rust:");
 
         let bindings = bindgen::Builder::default()
-            .header("c_binding/blschia.h")
-            // .header("c_binding/error.h")
-            .header("c_binding/elements.h")
-            .header("c_binding/privatekey.h")
-            .header("c_binding/schemes.h")
-            .header("c_binding/threshold.h")
-            // .header("c_binding/utils.hpp")
+            .header(c_binding_path.join("blschia.h").to_str().unwrap())
+            // .header(c_binding_path.join("error.h"))
+            .header(c_binding_path.join("elements.h").to_str().unwrap())
+            .header(c_binding_path.join("privatekey.h").to_str().unwrap())
+            .header(c_binding_path.join("schemes.h").to_str().unwrap())
+            .header(c_binding_path.join("threshold.h").to_str().unwrap())
+            // .header(c_binding_path.join("utils.h"))
             // .clang_arg("-xc++")
             .size_t_is_usize(true)
             // /Library/Developer/CommandLineTools/SDKs/MacOSX12.3.sdk/usr/include/c++/v1;
@@ -224,4 +220,8 @@ fn main() {
             .write_to_file(out_path)
             .expect("Couldn't write bindings!");
     }
+
+    // Rerun build if files changed
+    println!("cargo:rerun-if-changed={}", c_binding_path.display());
+    println!("cargo:rerun-if-changed={}", bls_dash_src_path.display());
 }
