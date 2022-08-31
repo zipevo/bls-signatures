477c014bb7d6ba3bd550bc3297be840481ac59af
Ivan Shumkov <ivan@shumkov.ru>
2022-08-31 23:19:54 +0300

    refactor: increase `c_err_to_result` readability

--- rust-bindings/bls-signatures/src/utils.rs
+++ rust-bindings/bls-signatures/src/utils.rs
@@ -9,11 +9,14 @@ where
     F: FnOnce(&mut bool) -> T,
 {
     let mut did_error = false;
+
     let result = f(&mut did_error);
+
     if did_error {
+        let error_message = unsafe { CStr::from_ptr(GetLastErrorMsg()) };
+
         Err(BlsError {
-            msg: String::from_utf8_lossy(unsafe { CStr::from_ptr(GetLastErrorMsg()) }.to_bytes())
-                .into_owned(),
+            msg: String::from_utf8_lossy(error_message.to_bytes()).into_owned(),
         })
     } else {
         Ok(result)
