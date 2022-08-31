c04c2941db53a9c892ec0edacb9469ae280080b1
Evgeny Fomin <fominok@hotmail.com>
2022-08-31 22:59:43 +0300

    safe wrappers for simple case

--- rust-bindings/bls-signatures/src/lib.rs
+++ rust-bindings/bls-signatures/src/lib.rs
@@ -1,12 +1,12 @@
 mod elements;
 mod private_key;
-mod utils;
 mod schemes;
+mod utils;
+
+use std::{error::Error, fmt::Display};
 
 pub use elements::{G1Element, G2Element};
 pub use private_key::PrivateKey;
-
-use std::{error::Error, fmt::Display};
 
 #[derive(Debug)]
 pub struct BlsError {
@@ -22,3 +22,48 @@ impl Display for BlsError {
 }
 
 impl Error for BlsError {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::schemes::{AugSchemeMPL, Scheme};
+
+    #[test]
+    fn basic_sign() {
+        let seed = b"seedweedseedweedseedweedseedweed";
+        let bad_seed = b"weedseedweedseedweedseedweedseed";
+
+        let scheme = AugSchemeMPL::new();
+        let private_key =
+            PrivateKey::key_gen(&scheme, seed).expect("unable to generate private key");
+        let public_key = private_key
+            .get_g1_element()
+            .expect("unable to get public key");
+
+        let private_key_bad =
+            PrivateKey::key_gen(&scheme, bad_seed).expect("unable to generate private key");
+        let public_key_bad = private_key_bad
+            .get_g1_element()
+            .expect("unable to get public key");
+
+        let message = b"Evgeny owns 1337 dash no cap";
+
+        let signature = scheme.sign(&private_key, message);
+        let verify = scheme.verify(&public_key, message, &signature);
+        assert!(verify);
+        let verify_bad = scheme.verify(&public_key_bad, message, &signature);
+        assert!(!verify_bad);
+    }
+
+    #[test]
+    fn bad_seed() {
+        let seed = b"lol";
+        let scheme = AugSchemeMPL::new();
+        let private_key = PrivateKey::key_gen(&scheme, seed);
+
+        assert!(matches!(
+            private_key,
+            Err(BlsError { msg }) if msg == "Seed size must be at least 32 bytes"
+        ));
+    }
+}
