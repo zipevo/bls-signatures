136d762607ee12b42943261f315cd192f31f9290
pankcuf <rastacake@gmail.com>
2023-03-20 02:04:41 +0400

    [fix: target_arch]

--- rust-bindings/bls-dash-sys/build.rs
+++ rust-bindings/bls-dash-sys/build.rs
@@ -17,7 +17,7 @@ fn create_cross_cmake_command() -> Command {
     if target_arch.eq("wasm32") {
         command.arg("cmake");
     }
-    if target.contains("apple-ios") {
+    if target_arch.contains("apple-ios") {
         command.arg("-DCMAKE_SYSTEM_NAME=iOS");
     }
 
